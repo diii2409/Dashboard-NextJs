4aa18d9ff8e56c4ad91c7234b9fd80b69703901b
diii2409 <duynhut366@gmail.com>
2024-12-14 02:26:46 +0700

    finish tutorial

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,4 +1,4 @@
-import {Revenue} from "@/app/lib/definitions";
+import {fetchRevenue} from "@/app/lib/data";
 import {generateYAxis} from "@/app/lib/utils";
 import {lusitana} from "@/app/ui/fonts";
 import {CalendarIcon} from "@heroicons/react/24/outline";
@@ -9,7 +9,9 @@ import {CalendarIcon} from "@heroicons/react/24/outline";
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart({revenue}: {revenue: Revenue[]}) {
+export default async function RevenueChart() {
+	// await new Promise(resolve => setTimeout(resolve, 3000));
+	const revenue = await fetchRevenue();
 	const chartHeight = 350;
 	// NOTE: Uncomment this code in Chapter 7
 
